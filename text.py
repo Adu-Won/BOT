e8a39ea889d627bfc57fb91e6819591e06eeead0
Adu-Won <96420639+Adu-Won@users.noreply.github.com>
2022-01-20 11:43:29 +0300

    Add files via upload

---
+++ text.py
@@ -0,0 +1,251 @@
+TEXT = """Applied Mathematics I 👉 /course_Math1101
+
+General Physics-I 👉 /course_Phys1101
+
+General Chemistry 👉 /course_Chem1101 
+
+Introduction to Computing 👉 /course_CSE1101 
+
+Communicative English Skills 👉 /course_ENG1011
+
+Civic and Ethical Education 👉 /course_LAR1011
+
+Health &Physical Education I 👉 /course_HPEd1011
+
+Logic and Critical Thinking 👉 /course_LAR1012"""
+
+TEXT2 = """Applied Mathematics II 👉 /course_Math1102
+
+General Physics II 👉 /course_Phys1102
+
+Fundamentals of Programming 👉 /course_CSE1102
+
+Engineering Drawing 👉 /course_DME1102
+
+Basic Writing Skills 👉 /course_ENG1102
+
+Health &Physical Education II 👉 /course_HPEd1012
+
+Logic and Critical Thinking 👉 /course_LAR1012
+
+Civic and Ethical Education 👉 /course_LAR1011
+"""
+TEXT3 = """
+Data Structure and Algorithm 👉 /course_CSE2101
+
+Electronic Circuit 👉 /course_ECE2101                
+
+Applied Mathematics III 👉 /course_MATH205
+
+Fundamental of Electrical Engineering 👉 /course_PCE2101
+
+Principle of Economics 👉 /course_SOS311
+"""
+TEXT4 = """
+
+Object Oriented Programming 👉 /course_CSE2202
+
+Discrete Mathematics for CSE 👉 /course_CSE2206
+
+Digital Logic Design 👉 /course_ECE3204
+
+Database Systems 👉 /course_CSE3207
+
+Electronic circuit II 👉 /course_ECE2202
+
+System Programming 👉 /course_CSE2320
+
+Computer Graphics 👉 /course_CSE3310
+"""
+TEXT5 = """
+Algorithms 👉 /course_CSE3211
+
+Probability and Random Process 👉 /course_ECE3103
+
+Fund. of software Engineering 👉 /course_CSE3205
+
+Computer Architecture & Organization 👉 /course_CSE3203
+"""
+TEXT6 = """
+Operating System 👉 /course_CSE3204
+
+Data Communication & Computer Networks 👉 /course_CSE3221
+
+Introduction to Artificial 👉 /course_CSE3206
+
+SOftware Requirement Engineering 👉 /course_CSE3308
+
+Web Programming 👉 /course_CSE3306
+
+Advanced Programming 👉 /course_CSE3312
+"""
+TEXT7 = """
+Formal Language and automata Theory 👉 /course
+
+Engineering Research and Development Methodology 👉 /course_CSE4221
+
+Multimedia Tech 👉 /course_CSE4303
+
+Software Design and Architecture 👉 /course_CSE4309
+
+Mobile Computing and Applications👉 /course_CSE4311
+
+Signals and Systems👉 /course_ECE2204
+
+Introduction to Data mining👉 /course_CSE5317
+
+Introduction to NLP👉 /course_CSE5321
+
+Computer System and security👉 /course_Unkown
+"""
+TEXT8 = """
+Programming languages 👉 /course_CSE4202
+
+Project Management 👉 /course_CSE4302
+
+Introduction to Law 👉 /course_Unknown
+
+Compiler Design 👉 /course_CSE4310
+
+Digital Signal Processing 👉 /course_ECE3205
+
+VLSI Design 👉 /course_ECE5307
+
+Electrical Network Analysis and synthesis 👉 /course_PCE3201
+
+Introduction to Computer Vision 👉 /course_CSE4312"""
+TEXT9 = """
+Semester Project 👉 /course_CSE5201
+
+Seminar 👉 /course_CSE5205
+
+Distributed Systems 👉 /course_CSE5307
+
+Wireless mobile Networks 👉 /course_CSE5309
+
+Image Processing 👉 /course_CSE5311
+
+Human Computer Interaction 👉 /course_CSE5313
+
+Introduction to Audio & Video Production 👉 /course_CSE5315
+
+Advanced Network 👉 /course_CSE5319
+
+Introduction to Control Systems 👉 /course_PCE3204
+"""
+TEXT10 = '''
+B.Sc. Project 👉 /course_CSE5202
+
+Entrepreneurship for Engineers 👉 /course_SOSC412
+
+Computer Games & Animation 👉 /course_CSE5304
+
+Special Topics in Computer Science & Engineering 👉 /course_CSE5306
+
+Real time and Embedded Systems 👉 /course_CSE5308
+
+Software Quality & Testing 👉 /course_CSE5310
+
+Computer Ethics and Social Issues 👉 /course_CSE5312
+
+Introduction to Robotics and Industrial Automation 👉 /course_PCE5308
+'''
+li = ["Applied Mathematics I Math1101",
+
+      'General Physics-I Phys1101',
+
+      'General Chemistry Chem1101',
+
+      'Introduction to Computing CSE1101',
+
+      'Communicative English Skills ENG1011',
+
+      'Civic and Ethical Education LAR1011',
+
+      'Health &Physical Education I HPEd1011',
+
+      'Logic and Critical Thinking LAR1012',
+
+      'Applied Mathematics II Math1102',
+
+      'General Physics II Phys1102',
+
+      'Fundamentals of Programming CSE1102',
+
+      'Engineering Drawing DME1102',
+
+      'Basic Writing Skills ENG1102',
+
+      'Health & Physical Education II HPEd1012',
+
+      'Logic and Critical Thinking LAR1012',
+
+      'Civic and Ethical Education LAR1011',
+
+      'Data Structure and Algorithm CSE2101',
+
+      'Electronic Circuit ECE2101',
+
+      'Applied Mathematics III MATH205',
+
+      'Fundamental of Electrical Engineering PCE2101',
+
+      'Principle of Economics SOS311',
+
+      'Object Oriented Programming CSE2202',
+
+      'Discrete Mathematics for CSE CSE2206',
+
+      'Digital Logic Design ECE3204',
+      'Database Systems CSE3207',
+      'Electronic circuit II ECE2202',
+      'System Programming CSE2320',
+      'Computer Graphics CSE3310',
+
+      'Algorithms CSE3211',
+      'Probability and Random Process ECE3103',
+      'Fund. of software Engineering CSE3205',
+      'Computer Architecture & Organization CSE3203',
+
+      'Operating System CSE3204',
+      'Data Communication & Computer Networks CSE3221',
+      'Introduction to Artificial CSE3206',
+      'SOftware Requirement Engineering CSE3308',
+      'Web Programming CSE3306',
+      'Advanced Programming CSE3312',
+
+      'Formal Language and automata Theory',
+      'Engineering Research and Development Methodology CSE4221',
+      'Multimedia Tech CSE4303',
+      'Software Design and Architecture CSE4309',
+      'Mobile Computing and ApplicationsCSE4311',
+      'Signals and SystemsECE2204',
+      'Introduction to Data miningCSE5317',
+      'Introduction to NLPCSE5321',
+      'Computer System and securityUnkown',
+
+      'Programming languages CSE4202',
+      'Project Management CSE4302',
+      'Introduction to Law Unknown',
+      'Compiler Design CSE4310',
+      'Digital Signal Processing ECE3205',
+      'VLSI Design ECE5307',
+      'Electrical Network Analysis and synthesis PCE3201',
+      'Introduction to Computer Vision CSE4312',
+      'Semester Project CSE5201',
+      'Seminar CSE5205',
+      'Distributed Systems CSE5307',
+      'Wireless mobile Networks CSE5309',
+      'Image Processing CSE5311',
+      'Human Computer Interaction CSE5313',
+      'Introduction to Audio & Video Production CSE5315',
+      'Advanced Network CSE5319',
+      'Introduction to Control Systems PCE3204',
+      'B.Sc. Project CSE5202',
+      'Entrepreneurship for Engineers SOSC412',
+      'Computer Games & Animation CSE5304',
+      'Special Topics in Computer Science & Engineering CSE5306',
+      'Real time and Embedded Systems CSE5308',
+      'Software Quality & Testing CSE5310',
+      'Computer Ethics and Social Issues CSE5312',
+      'Introduction to Robotics and Industrial Automation PCE5308']
